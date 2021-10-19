e1d1fd0f2d1f9ecc92e23755184fbd38b1b88fcb
Joseph Surla <josephsurla@gmail.com>
2021-10-19 12:41:29 +0900

    connect wallet to web app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,59 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  // Render Method
-  const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
-      Connect to Wallet
-    </button>
-  );
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+    // Checks if authorized to access the user's wallet.
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    // Gets first account if user has multiple authorized accounts.
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  };
+
+  // Implement connectWallet method to allow users to connect their wallet.
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      // Request access to account
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      // Prints public address once user authorizes Metamask.
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
@@ -15,7 +62,18 @@ function App() {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {renderNotConnectedContainer()}
+          {currentAccount === "" ? (
+            <button
+              onClick={connectWallet}
+              className="cta-button connect-wallet-button"
+            >
+              Connect to Wallet
+            </button>
+          ) : (
+            <button onClick={null} className="cta-button connect-wallet-button">
+              Mint NFT
+            </button>
+          )}
         </div>
       </div>
     </div>
